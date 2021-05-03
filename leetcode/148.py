44a385dae4c234a61540efa64a835a1e23eefd18
stay7 <codeLatte20@gmail.com>
2021-05-03 17:06:07 +0900

    Add solution 148.Sort List

--- leetcode/148.py
+++ leetcode/148.py
@@ -13,9 +13,34 @@ class ListNode:
 
 
 # 제일 빠르면서 간단한 linked list -> python array
+# Runtime 544 ms (24.81%)
+# Memory 50.9 MB (6.40%)
+class Solution:
+    def merge(self, l1: ListNode, l2: ListNode):
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.merge(l1.next, l2)
+        return l1 or l2
+
+    def sortList(self, head: ListNode) -> ListNode:
+        if not (head and head.next):
+            return head
+
+        slow = fast = head
+        while slow and fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+        return self.merge(l1, l2)
+
+
+# 제일 빠르면서 간단한 linked list -> python array
 # Runtime 148ms (98.49%)
 # Memory 30.1MB (89.02%)
-class Solution:
+class Solution1:
     def sortList(self, head: ListNode) -> ListNode:
         root = head
         nums = []
