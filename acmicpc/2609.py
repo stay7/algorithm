84ef39a2b8dccbb17dfaf91660335d6b35ef42cb
stay7 <codeLatte20@gmail.com>
2021-07-31 21:33:58 +0900

    Add solution 2609

--- acmicpc/2609.py
+++ acmicpc/2609.py
@@ -3,8 +3,24 @@
 2609. 최대공약수와 최소공배수
 """
 
+# math 라이브러리
 import math
 
 A, B = map(int, input().split())
 print(math.gcd(A, B))
 print(math.lcm(A, B))
+
+
+# 유클리드 호제법
+def sol():
+    def gcd(A, B):
+        if B == 0:
+            return A
+        return gcd(B, A % B)
+
+    A, B = map(int, input().split())
+    if A < B:
+        A, B = B, A
+    divistor = gcd(A, B)
+    print(divistor)
+    print(int(A*B/divistor))
