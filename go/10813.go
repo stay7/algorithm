e6a243ba3f77f334303894c72e15be8527f5420b
stay7 <codelatte20@gmail.com>
2023-09-06 00:12:31 +0900

    10813.go

---
+++ go/10813.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var N, M int
+	fmt.Scanln(&N, &M)
+
+	buckets := make([]string, N)
+	for i := 0; i < N; i++ {
+		buckets[i] = strconv.Itoa(i + 1)
+	}
+
+	for i := 0; i < M; i++ {
+		var a, b int
+		fmt.Scanln(&a, &b)
+		swap(&buckets[a-1], &buckets[b-1])
+	}
+
+	fmt.Println(strings.Join(buckets, " "))
+}
+
+func swap(a *string, b *string) {
+	tmp := *b
+	*b = *a
+	*a = tmp
+}
